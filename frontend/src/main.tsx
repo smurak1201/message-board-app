7681d44c386461aa8bb515688725ab188322e2d9
smurak1201 <s.murakoshi1201@gmail.com>
2025-06-19 11:51:43 +0900

    fix: Providerのインポートパスを修正

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { Provider } from "@/components/ui/provider";
+import { Provider } from "./components/ui/provider";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
